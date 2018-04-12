d0deda1188bc697f3f4863553ff59d9beae8e0b3
KentAgent <georgios.apostolidis92@gmail.com>
2018-04-12 22:57:50 +0200

    Initial commit

---
+++ Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwissKnife
+//
+//  Created by Georgios on 2018-04-12.
+//  Copyright © 2018 Georgios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
