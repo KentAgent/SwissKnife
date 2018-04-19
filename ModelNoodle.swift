b7c6229e7f6f52dde428cece0210f05db86836eb
WilliamLennartsson <31884043+WilliamLennartsson@users.noreply.github.com>
2018-04-19 15:20:27 +0200

    Setup

---
+++ ModelNoodle.swift
@@ -0,0 +1,9 @@
+//
+//  ModelNoodle.swift
+//  SwissKnife
+//
+//  Created by will on 2018-04-19.
+//  Copyright © 2018 Georgios. All rights reserved.
+//
+
+import Foundation
---
+++ Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  SwissKnife
+//
+//  Created by will on 2018-04-19.
+//  Copyright © 2018 Georgios. All rights reserved.
+//
+
+import SpriteKit
+
+class Player: SKSpriteNode {
+
+}
