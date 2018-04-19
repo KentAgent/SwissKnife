96cb6e60f22bb2d3811a25c97a0f778f56491e26
Mani Sedighi <manisedighi@Apples-MacBook-Pro.local>
2018-04-19 12:12:50 +0200

    added outlets

--- Controller/TicTacToeViewController.swift
+++ Controller/TicTacToeViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class TicTacToeViewController: UIViewController {
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    @IBOutlet weak var Button10: UIButton!
+    @IBOutlet weak var Button11: UIButton!
+    @IBOutlet weak var Button12: UIButton!
+    @IBOutlet weak var Button20: UIButton!
+    @IBOutlet weak var Button21: UIButton!
+    @IBOutlet weak var Button22: UIButton!
+    @IBOutlet weak var Button30: UIButton!
+    @IBOutlet weak var Button31: UIButton!
+    @IBOutlet weak var Button32: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,21 +29,41 @@ class TicTacToeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnPressed1(_ sender: UIButton) {
+        print("pressed 1.0")
     }
     
-    // Hey this is Mani
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnPressed2(_ sender: UIButton) {
+        print("pressed 1.1")
     }
-    */
-
+    
+    @IBAction func btnPressed3(_ sender: UIButton) {
+        print("pressed 1.3")
+    }
+    
+    @IBAction func btnPressed4(_ sender: UIButton) {
+        print("pressed 2.0")
+    }
+    
+    @IBAction func btnPressed5(_ sender: UIButton) {
+        print("pressed 2.1")
+    }
+    
+    @IBAction func btnPressed6(_ sender: UIButton) {
+        print("pressed 2.2")
+    }
+    
+    @IBAction func btnPressed7(_ sender: UIButton) {
+        print("pressed 3.0")
+    }
+    
+    @IBAction func btnPressed8(_ sender: UIButton) {
+        print("pressed 3.1")
+    }
+    
+    @IBAction func btnPressed9(_ sender: UIButton) {
+        print("pressed 3.2")
+    }
+    
+    
 }
