59beed61ca1bffc65a4a77045c2745ab736864e5
Robert <roberteriksson12@gmail.com>
2018-04-19 12:31:50 +0200

    ta mina updates

--- AnnotationModel.swift
+++ AnnotationModel.swift
@@ -6,8 +6,25 @@
 //  Copyright © 2018 Georgios. All rights reserved.
 //
 
-import UIKit
+import MapKit
 
-class AnnotationModel: NSObject {
+class AnnotationModel: NSObject, MKAnnotation {
 
+    let title: String?
+    let locationName: String
+    let dicipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, dicipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.dicipline = dicipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
 }
--- Controller/MapViewController.swift
+++ Controller/MapViewController.swift
@@ -8,20 +8,50 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        setupLocationManager()
         setupMapView()
+        testAnnotation()
+    }
+    
+    func setupLocationManager() {
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        
+        let status = CLLocationManager.authorizationStatus()
+        if status == .notDetermined || status == .denied || status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
     }
     
     func setupMapView() {
-        
+        mapView.delegate = self
+        mapView.mapType = MKMapType(rawValue: 0)!
+        mapView.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
+        mapView.showsUserLocation = true
+    }
+    
+    func testAnnotation() {
+        let annotation = AnnotationModel(title: "mamma",
+                                         locationName: "här",
+                                         dicipline: "Fint",
+                                         coordinate: CLLocationCoordinate2D(latitude: mapView.userLocation.coordinate.latitude, longitude: mapView.userLocation.coordinate.longitude))
+        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
